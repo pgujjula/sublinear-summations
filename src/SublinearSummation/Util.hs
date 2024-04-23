ee32d561891b85991ad15defec45792357b09a3f
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-04-23 14:20:56 -0500

    Add SublinearSummation.Util

---
+++ src/SublinearSummation/Util.hs
@@ -0,0 +1,12 @@
+-- SPDX-FileCopyrightText: Copyright Preetham Gujjula
+-- SPDX-License-Identifier: BSD-3-Clause
+
+module SublinearSummation.Util (word2Int, int2Word) where
+
+word2Int :: Word -> Int
+word2Int = fromIntegral
+{-# INLINE word2Int #-}
+
+int2Word :: Int -> Word
+int2Word = fromIntegral
+{-# INLINE int2Word #-}
