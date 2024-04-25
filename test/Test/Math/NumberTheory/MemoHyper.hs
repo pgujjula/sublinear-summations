a272ada24c2a65028e28fc3c035dfd77102e242b
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-04-25 13:53:13 -0500

    Add test for memoHyperMertens

--- test/Test/Math/NumberTheory/MemoHyper.hs
+++ test/Test/Math/NumberTheory/MemoHyper.hs
@@ -3,8 +3,12 @@
 
 module Test.Math.NumberTheory.MemoHyper (tests) where
 
+import Control.Monad (forM_)
+import Math.NumberTheory.MemoHyper (UMemoHyper, memoHyperMertens, unMemoHyper)
+import SublinearSummation.Util (primes)
 import Test.Tasty (TestTree, testGroup)
-import Test.Util (todoTest)
+import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Util (todoCode, todoTest)
 
 tests :: TestTree
 tests =
@@ -96,10 +100,44 @@ memoHyperPrimeSumTests = todoTest "memoHyperPrimeSum"
 -- Square-free integers
 
 memoHyperMertensTests :: TestTree
-memoHyperMertensTests = todoTest "memoHyperMertens"
+memoHyperMertensTests =
+  todoCode $
+    testCase "memoHyperMertens" $
+      forM_ [1 .. 100] $ \n ->
+        let mh :: UMemoHyper Int
+            mh = memoHyperMertens n
+         in forM_ [1 .. n] $ \i ->
+              unMemoHyper mh i @?= mertensNaive (fromIntegral (n `quot` i))
 
 memoHyperNumSquarefreeTests :: TestTree
 memoHyperNumSquarefreeTests = todoTest "memoHyperNumSquarefree"
 
 memoHyperSumSquarefreeTests :: TestTree
 memoHyperSumSquarefreeTests = todoTest "memoHyperSumSquarefree"
+
+--
+-- Utilities
+--
+mertensNaive :: (Integral a) => a -> a
+mertensNaive n = sum (map mobiusNaive [1 .. n])
+
+isSquarefulNaive :: (Integral a) => a -> Bool
+isSquarefulNaive n = any (`divides` n) (takeWhile (<= n) squares)
+
+squares :: (Integral a) => [a]
+squares = map (^ (2 :: Int)) [2 ..]
+
+divides :: (Integral a) => a -> a -> Bool
+divides a b = (b `rem` a) == 0
+
+mobiusNaive :: (Integral a) => a -> a
+mobiusNaive 0 = 0
+mobiusNaive n =
+  if isSquarefulNaive n
+    then 0
+    else
+      let ps = takeWhile (<= n) (map fromIntegral primes)
+          primeDivisors = filter (`divides` n) ps
+       in if even (length primeDivisors)
+            then 1
+            else -1
