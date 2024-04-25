e062b21123aa78088be70ba679768e4510dd6f88
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-04-24 20:32:38 -0500

    Add implementation of primes/primesFrom

--- src/SublinearSummation/Util.hs
+++ src/SublinearSummation/Util.hs
@@ -1,9 +1,14 @@
 -- SPDX-FileCopyrightText: Copyright Preetham Gujjula
 -- SPDX-License-Identifier: BSD-3-Clause
 
-module SublinearSummation.Util (word2Int, int2Word) where
+module SublinearSummation.Util (word2Int, int2Word, primes, primesFrom) where
 
-import Control.Placeholder (todo)
+import Data.Vector.Storable (Vector)
+import Data.Vector.Storable qualified as Vector
+import Data.Word (Word64)
+import Math.Prime.FastSieve (generatePrimes)
+import System.IO.Unsafe (unsafePerformIO)
+import Unsafe.Coerce (unsafeCoerce)
 
 word2Int :: Word -> Int
 word2Int = fromIntegral
@@ -13,5 +18,20 @@ int2Word :: Int -> Word
 int2Word = fromIntegral
 {-# INLINE int2Word #-}
 
-primes :: [Int]
-primes = todo
+primes :: [Word]
+primes = primesFrom 0
+
+primesFrom :: Word -> [Word]
+primesFrom i =
+  let starts :: [Word]
+      starts = map (+ i) (0 : iterate (* 2) 1)
+
+      ends :: [Word]
+      ends = map (\x -> x - 1) (tail starts)
+   in concatMap Vector.toList (zipWith primesVec starts ends)
+
+primesVec :: Word -> Word -> Vector Word
+primesVec n m =
+  let v :: Vector Word64
+      v = unsafePerformIO $ generatePrimes (fromIntegral n) (fromIntegral m)
+   in unsafeCoerce v
