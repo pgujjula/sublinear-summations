bb8553971c075ed43e8cdbb150c442a291083d89
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-04-25 00:00:06 -0500

    Add tests for sigma, diff, mul

--- test/Test/Math/NumberTheory/HyperbolicConvolution.hs
+++ test/Test/Math/NumberTheory/HyperbolicConvolution.hs
@@ -3,8 +3,9 @@
 
 module Test.Math.NumberTheory.HyperbolicConvolution (tests) where
 
-import Math.NumberTheory.HyperbolicConvolution (hyper)
+import Math.NumberTheory.HyperbolicConvolution (diff, hyper, mul, sigma)
 import Math.NumberTheory.Roots (integerSquareRoot)
+import SublinearSummation.Util (word2Int)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 import Test.Util (todoTest)
@@ -45,13 +46,41 @@ hyperTests =
     ]
 
 sigmaTests :: TestTree
-sigmaTests = todoTest "sigma"
+sigmaTests =
+  testGroup
+    "sigma"
+    [ testCase "sigma id" $
+        map (sigma id) [1 .. 10]
+          @?= [1, 3, 6, 10, 15, 21, 28, 36, 45, 55],
+      testCase "sigma (hyper 10 id)" $ do
+        sigma (hyper 10 id) 10 @?= 27
+        sigma (hyper 10 id) 5 @?= 22
+        sigma (hyper 5 id) 5 @?= 10
+    ]
 
 diffTests :: TestTree
-diffTests = todoTest "diff"
+diffTests =
+  testGroup
+    "diff"
+    [ testCase "diff id" $
+        map (diff id) [1 .. 10]
+          @?= replicate 10 1,
+      testCase "diff (hyper n word2Int)" $
+        map (diff (hyper 10 word2Int)) [1 .. 10]
+          @?= [10, -5, -2, -1, 0, -1, 0, 0, 0, 0]
+    ]
+
+square :: (Integral a) => a -> a
+square x = x * x
 
 mulTests :: TestTree
-mulTests = todoTest "mul"
+mulTests =
+  testGroup
+    "mul"
+    [ testCase "mul id id" $
+        map (mul id id) [1 .. 10]
+          @?= map square [1 .. 10]
+    ]
 
 --
 -- Computing hyperbolic convolutions
