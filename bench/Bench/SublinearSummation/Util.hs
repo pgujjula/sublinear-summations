0d5bf16fc012e9a00e4a2dbf49c9b0a14da4ca2f
Preetham Gujjula <gitcommit@mail.preetham.io>
2024-04-29 01:09:26 -0700

    Add benchmark for primeSumVec

--- bench/Bench/SublinearSummation/Util.hs
+++ bench/Bench/SublinearSummation/Util.hs
@@ -3,17 +3,24 @@
 
 module Bench.SublinearSummation.Util (benchmarks) where
 
-import SublinearSummation.Util (primesFrom)
+import Data.Vector.Generic qualified as Vector
+import SublinearSummation.Util (primeSumVec, primesFrom)
 import Test.Tasty.Bench (Benchmark, bench, bgroup, nf)
 
 benchmarks :: Benchmark
 benchmarks =
   bgroup
     "SublinearSummation.Util"
-    [primesToBenchmarks]
+    [primesToBenchmarks, primeSumVecBenchmarks]
 
 primesToBenchmarks :: Benchmark
 primesToBenchmarks =
   bgroup "primesTo" $ flip map [(1 :: Int) .. 8] $ \i ->
     bench ("10^" ++ show i) $
       nf ((!! (10 ^ i)) . primesFrom) (10 ^ i)
+
+primeSumVecBenchmarks :: Benchmark
+primeSumVecBenchmarks =
+  bgroup "primeSumVec" $ flip map [(1 :: Int) .. 8] $ \i ->
+    bench ("10^" ++ show i) $
+      nf (Vector.last . primeSumVec) (10 ^ i)
